5ec2b2f12cd08522acd6d9d28457b84b9c56bad5
HashiruG <hashiruhgunathilake@gmail.com>
2024-11-03 23:12:50 +0530

    Define collections

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from db import getDatabase
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+
+database = getDatabase()
+
+
+project_collection = database["projects"]
+programming_skills_collection = database["programming_skills"]
+web_skills_collection = database["web_skills"]
+machine_learning_skills_collection = database["machine_learning_skills"]
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
